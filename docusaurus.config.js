b72f6cb956d602c01e3060e376576997c41c3def
Alexander <a.lokhanov@gmail.com>
2023-11-22 02:52:43 +0300

    edit config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'KITP learn site',
+  title: 'KITP training site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'KITP learn site',
+        title: 'KITP training site',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
