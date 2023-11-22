f4faaa6edaf568da1f6cd74a6e8b4dd0eb96891f
Alexander Lokhanov <a.lokhanov@gmail.com>
2023-11-22 03:04:56 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/VLSU-KITP',
             label: 'GitHub',
             position: 'right',
           },
